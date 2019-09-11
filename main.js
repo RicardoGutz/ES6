d8f04aca37d7212313fb6f7a3ea1e4625d70814b
RicardoGutz <rickgutz99@gmail.com>
2019-09-11 00:11:51 -0300

    Learning Template Literals & Object Short Syntax

--- main.js
+++ main.js
@@ -1,45 +1,20 @@
-// REST
+//Tenplate Literals
+
+const nome = "Ricardo";
+const idade = 19;
+
+//console.log("Meu nome é " + nome + " e tenho " + idade + " anos.");
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
+
+// Object Short Syntax
+
+const sobrenome = "Gutz";
+const altura = 167;
 
 const usuario = {
-  nome: "Ricardo",
-  idade: 19,
+  nome,
+  idade,
   empresa: "Alternative Agência"
 };
 
-const { nome, ...resto } = usuario;
-
-console.log("Nome: " + nome);
-console.log("Resto: " + resto);
-
-const arr = [1, 2, 3, 4];
-
-const [a, b, ...c] = arr;
-
-console.log(a);
-console.log(b);
-console.log(c);
-
-function soma(...params) {
-  return params.reduce((total, next) => total + next);
-}
-
-console.log(soma(1, 3, 4));
-
-//SPREAD
-
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-
-const arr3 = [...arr1, ...arr2];
-
-console.log(arr3);
-
-const usuario1 = {
-  nome: "Diego",
-  idade: 19,
-  empresa: "Alternative Agência"
-};
-
-const usuario2 = { ...usuario1, nome: "Ricardo" };
-
-console.log(usuario2);
+console.log(usuario);
