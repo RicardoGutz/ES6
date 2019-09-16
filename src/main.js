c0790244bf0234343089652aa55f6280521ab862
RicardoGutz <rickgutz99@gmail.com>
2019-09-15 22:31:28 -0300

    Learning about async and await

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
-alert("Opa");
-alert("heuehueheu");
+const minhaPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("OK");
+    }, 2000);
+  });
+
+async function executaPromise() {
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+}
+
+executaPromise();
